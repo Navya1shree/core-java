6ff9832c07a5b1f3415145b2578caeb4769928e4
Navya1shree <navyashreeb43gmail.com>
2026-03-29 19:52:39 +0530

    tasktask 09-03-26

---
+++ FavoriteFoods.java
@@ -0,0 +1,17 @@
+class FavoriteFoods{
+	public static void main(String[]agrs){
+	
+	String favoriteFoods[]={"masal dosa","poori","Chiken biriyani","idli","bisibele bath"};
+
+    System.out.println("FavoriteFoods:"+favoriteFoods[4]);
+    System.out.println("trainee FavoriteFoods:"+favoriteFoods[2]);
+    System.out.println("FavoriteFoods:"+favoriteFoods[3]);
+    System.out.println("FavoriteFoods:"+favoriteFoods[0]);
+	    System.out.println("FavoriteFoods:"+favoriteFoods[5]);
+	    System.out.println("Length of array:"+favoriteFoods.length);
+
+
+	}
+
+
+}
