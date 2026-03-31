b014328d6ef68db88d944e5fb1a82396f65e5498
Navya1shree <navyashreeb43gmail.com>
2026-03-31 16:55:38 +0530

    30-03-26

--- Calculater.java
+++ Calculater.java
@@ -1,20 +1,125 @@
-class Calculater{
-	public void add(int a,int b){
-		int sum = a + b;
+class Calculater {
 
-		
-	System.out.println("Sum of the numbers: " + a + " and " + b + " is: " + sum);
+   
+    public void add(int a1) {
+        System.out.println("Add: " + a1);
+    }
 
-	}
+    public void add(int a1, int a2) {
+        System.out.println("Add: " + (a1 + a2));
+    }
 
+    public void add(int a1, int a2, int a3) {
+        System.out.println("Add: " + (a1 + a2 + a3));
+    }
 
+    public void add(int a1, int a2, int a3, int a4) {
+        System.out.println("Add: " + (a1 + a2 + a3 + a4));
+    }
 
+    public void add(int a1, int a2, int a3, int a4, int a5) {
+        System.out.println("Add: " + (a1 + a2 + a3 + a4 + a5));
+    }
 
+    public void add(int a1, int a2, int a3, int a4, int a5, int a6) {
+        System.out.println("Add: " + (a1 + a2 + a3 + a4 + a5 + a6));
+    }
 
+    public void add(int a1, int a2, int a3, int a4, int a5, int a6, int a7) {
+        System.out.println("Add: " + (a1 + a2 + a3 + a4 + a5 + a6 + a7));
+    }
 
+    public void add(int a1, int a2, int a3, int a4, int a5, int a6, int a7, int a8) {
+        System.out.println("Add: " + (a1 + a2 + a3 + a4 + a5 + a6 + a7 + a8));
+    }
 
+    public void add(int a1, int a2, int a3, int a4, int a5, int a6, int a7, int a8, int a9) {
+        System.out.println("Add: " + (a1 + a2 + a3 + a4 + a5 + a6 + a7 + a8 + a9));
+    }
 
+    public void add(int a1, int a2, int a3, int a4, int a5, int a6, int a7, int a8, int a9, int a10) {
+        System.out.println("Add: " + (a1+a2+a3+a4+a5+a6+a7+a8+a9+a10));
+    }
 
+                  //sub  
+    public void sub(int a1) {
+        System.out.println("Sub: " + a1);
+    }
 
+    public void sub(int a1, int a2) {
+        System.out.println("Sub: " + (a1 - a2));
+    }
 
+    public void sub(int a1, int a2, int a3) {
+        System.out.println("Sub: " + (a1 - a2 - a3));
+    }
+
+    public void sub(int a1, int a2, int a3, int a4) {
+        System.out.println("Sub: " + (a1 - a2 - a3 - a4));
+    }
+
+    public void sub(int a1, int a2, int a3, int a4, int a5) {
+        System.out.println("Sub: " + (a1 - a2 - a3 - a4 - a5));
+    }
+
+    public void sub(int a1, int a2, int a3, int a4, int a5, int a6) {
+        System.out.println("Sub: " + (a1 - a2 - a3 - a4 - a5 - a6));
+    }
+
+    public void sub(int a1, int a2, int a3, int a4, int a5, int a6, int a7) {
+        System.out.println("Sub: " + (a1 - a2 - a3 - a4 - a5 - a6 - a7));
+    }
+
+    public void sub(int a1, int a2, int a3, int a4, int a5, int a6, int a7, int a8) {
+        System.out.println("Sub: " + (a1 - a2 - a3 - a4 - a5 - a6 - a7 - a8));
+    }
+
+    public void sub(int a1, int a2, int a3, int a4, int a5, int a6, int a7, int a8, int a9) {
+        System.out.println("Sub: " + (a1 - a2 - a3 - a4 - a5 - a6 - a7 - a8 - a9));
+    }
+
+    public void sub(int a1, int a2, int a3, int a4, int a5, int a6, int a7, int a8, int a9, int a10) {
+        System.out.println("Sub: " + (a1-a2-a3-a4-a5-a6-a7-a8-a9-a10));
+    }
+
+    //multiple
+    public void mul(int a1) {
+        System.out.println("Mul: " + a1);
+    }
+
+    public void mul(int a1, int a2) {
+        System.out.println("Mul: " + (a1 * a2));
+    }
+
+    public void mul(int a1, int a2, int a3) {
+        System.out.println("Mul: " + (a1 * a2 * a3));
+    }
+
+    public void mul(int a1, int a2, int a3, int a4) {
+        System.out.println("Mul: " + (a1 * a2 * a3 * a4));
+    }
+
+    public void mul(int a1, int a2, int a3, int a4, int a5) {
+        System.out.println("Mul: " + (a1 * a2 * a3 * a4 * a5));
+    }
+
+    public void mul(int a1, int a2, int a3, int a4, int a5, int a6) {
+        System.out.println("Mul: " + (a1 * a2 * a3 * a4 * a5 * a6));
+    }
+
+    public void mul(int a1, int a2, int a3, int a4, int a5, int a6, int a7) {
+        System.out.println("Mul: " + (a1 * a2 * a3 * a4 * a5 * a6 * a7));
+    }
+
+    public void mul(int a1, int a2, int a3, int a4, int a5, int a6, int a7, int a8) {
+        System.out.println("Mul: " + (a1 * a2 * a3 * a4 * a5 * a6 * a7 * a8));
+    }
+
+    public void mul(int a1, int a2, int a3, int a4, int a5, int a6, int a7, int a8, int a9) {
+        System.out.println("Mul: " + (a1 * a2 * a3 * a4 * a5 * a6 * a7 * a8 * a9));
+    }
+
+    public void mul(int a1, int a2, int a3, int a4, int a5, int a6, int a7, int a8, int a9, int a10) {
+        System.out.println("Mul: " + (a1*a2*a3*a4*a5*a6*a7*a8*a9*a10));
+    }
 }
